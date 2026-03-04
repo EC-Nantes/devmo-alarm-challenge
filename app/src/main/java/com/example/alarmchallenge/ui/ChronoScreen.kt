9645dbfed60a2c168b9aefb6de3ab5b49594635a
clement-vazeille <clement.vazeille@gmail.com>
2026-03-04 17:56:12 +0100

    Base UI without interactive components
    Added the main layout of the ai
    What's missing is the interactive components

--- app/src/main/java/com/example/alarmchallenge/ui/ChronoScreen.kt
+++ app/src/main/java/com/example/alarmchallenge/ui/ChronoScreen.kt
@@ -3,10 +3,17 @@ package com.example.alarmchallenge.ui
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawingPadding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme.colorScheme
@@ -16,6 +23,9 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -24,11 +34,50 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+
+import com.example.alarmchallenge.R
+import com.example.alarmchallenge.ui.theme.AlarmChallengeTheme
 
 @Composable
-fun ChronoLayout(
-) {
+fun GameScreen(gameViewModel: GameViewModel = viewModel()) {
+    val gameUiState by gameViewModel.uiState.collectAsState()
+    val mediumPadding = dimensionResource(R.dimen.padding_medium)
 
+    Column(
+        modifier = Modifier
+            .statusBarsPadding()
+            .verticalScroll(rememberScrollState())
+            .safeDrawingPadding()
+            .padding(mediumPadding),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        Text(
+            text = stringResource(R.string.app_name),
+            style = typography.titleLarge,
+        )
+
+        // Adding a Spacer of height 20dp
+        Spacer(modifier = Modifier.height(20.dp))
+
+        //Chrono displayed at the top
+        Text(
+            text =
+                gameUiState.currentTimerBaseMinutesValue.toString() + " : "
+                    + gameUiState.currentTimerBaseSecondsValue.toString(),
+            style = typography.titleLarge,
+        )
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+        ButtonLayout()
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+        TimeLayout()
+    }
 }
 
 @Composable
